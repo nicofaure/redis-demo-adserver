c19393948d5c4cb1165f7fb6a7231aadd548b53d
nfaure <nicolas.faure@mercadolibre.com.ar>
2015-07-14 00:44:55 -0300

    Add basic controller

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,19 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import redis.RedisClient;
+import spring.SpringApplicationContext;
+import views.html.index;
 
 public class Application extends Controller {
 
     public static Result index() {
+
+		// An example to test that redis client is working
+		RedisClient client = SpringApplicationContext.getBean(RedisClient.class);
+		client.set("key", "value");
+
         return ok(index.render("Your new application is ready."));
     }
 
